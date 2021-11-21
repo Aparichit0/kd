ce47b58b13e896914b126f3be72acb09f8708bd9
Lelouch vi britannia <93782780+Aparichit0@users.noreply.github.com>
2021-11-21 22:46:38 +0530

    auto play on mouseover or click

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 
 alert("⚠️Work in progress⚠️\n💻Open in desktop mode💻")
 alert("abhi ke liye isi se chala le, baki suggestions deta rahiyo teri party tak set kar dunga(our padhna padega😅)")
+
+window.addEventListener("mouseover",playsong);
+window.addEventListener("click", playsong);
+function playsong {
+document.getElementByID("song")play();
+}
