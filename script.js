73e1ebc61247d6ceddfdb46bdbeb0650ab40fb99
Aparichit0 <0.dharmveersingh@gmail.com>
2021-11-21 21:53:01 +0530

    update v1

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 
 alert("⚠️Work in progress⚠️\n💻Open in desktop mode💻")
-alert("abhi ke liye isi se chala le, baki teri party tak set kar dunga(our padhna padega😅)")
+alert("abhi ke liye isi se chala le, baki suggestions deta rahiyo teri party tak set kar dunga(our padhna padega😅)")
