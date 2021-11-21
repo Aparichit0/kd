4572a9541d6517f36f957d3855adccc83958c381
Aparichit0 <0.dharmveersingh@gmail.com>
2021-11-21 19:05:11 +0530

    update demo bg

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-//alert("⚠️Work in progress⚠️")
+
+alert("⚠️Work in progress⚠️\n💻Open in desktop mode💻")
+alert("abhi ke liye isi se chala le, baki teri party tak set kar dunga(our padhna padega😅)")
