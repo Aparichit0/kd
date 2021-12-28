86cd7cb8867fd3ba92c6b35e4ee0a6a0ae8aa92f
Lelouch vi britannia <93782780+Aparichit0@users.noreply.github.com>
2021-12-29 01:27:39 +0530

    flex arrangement fix
    & minor opacity change for playerIcon

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ var icon = document.getElementById("playerIcon");
 if (song.paused == true) {
 song.play();
 icon.setAttribute("name", "pause")
+icon.style.opacity="75%";
 } else {
 song.pause();
-icon.setAttribute("name", "play")
+icon.setAttribute("name", "play");
+icon.style.opacity="100%";
 }
 }
